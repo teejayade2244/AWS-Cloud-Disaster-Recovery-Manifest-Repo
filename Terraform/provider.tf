85e98df391dd4b1249a55f3009728b43fb25978b
TemitopeAde22 <temitope224468@gmail.com>
2025-08-07 22:02:11 +0100

    Added EKS Modules

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,4 +1,21 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" 
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23" 
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.2"
+    }
+  }
+}
+
 provider "aws" {
   region = var.primary_region
   alias  = "primary" # Alias to distinguish this provider instance
@@ -19,43 +36,43 @@ provider "tls" {
   alias = "secondary"
 }
 
-    # Configure the Kubernetes provider for the primary EKS cluster
-    # This provider's configuration will be dynamically set after the EKS cluster is created
-    # It needs a dummy config for `terraform init` to pass
-    provider "kubernetes" {
-      alias = "primary"
-      host                   = "https://dummy-host-primary"
-      cluster_ca_certificate = "dummy-ca-primary"
-      token                  = "dummy-token-primary"
-    }
+# Configure the Kubernetes provider for the primary EKS cluster
+# This provider's configuration will be dynamically set after the EKS cluster is created
+# It needs a dummy config for `terraform init` to pass
+provider "kubernetes" {
+  alias = "primary"
+  host                   = "https://dummy-host-primary"
+  cluster_ca_certificate = "dummy-ca-primary"
+  token                  = "dummy-token-primary"
+}
 
-    # Configure the Kubernetes provider for the secondary EKS cluster
-    provider "kubernetes" {
-      alias = "secondary"
-      host                   = "https://dummy-host-secondary"
-      cluster_ca_certificate = "dummy-ca-secondary"
-      token                  = "dummy-token-secondary"
-    }
+# Configure the Kubernetes provider for the secondary EKS cluster
+provider "kubernetes" {
+  alias = "secondary"
+  host                   = "https://dummy-host-secondary"
+  cluster_ca_certificate = "dummy-ca-secondary"
+  token                  = "dummy-token-secondary"
+}
 
-    # Configure the Helm provider for the primary EKS cluster
-    provider "helm" {
-      alias = "primary"
-      kubernetes {
-        host                   = "https://dummy-helm-host-primary"
-        cluster_ca_certificate = "dummy-helm-ca-primary"
-        token                  = "dummy-helm-token-primary"
-      }
-    }
+# Configure the Helm provider for the primary EKS cluster
+provider "helm" {
+  alias = "primary"
+  kubernetes {
+    host                   = "https://dummy-helm-host-primary"
+    cluster_ca_certificate = "dummy-helm-ca-primary"
+    token                  = "dummy-helm-token-primary"
+  }
+}
 
-    # Configure the Helm provider for the secondary EKS cluster
-    provider "helm" {
-      alias = "secondary"
-      kubernetes {
-        host                   = "https://dummy-helm-host-secondary"
-        cluster_ca_certificate = "dummy-helm-ca-secondary"
-        token                  = "dummy-helm-token-secondary"
-      }
-    }
+# Configure the Helm provider for the secondary EKS cluster
+provider "helm" {
+  alias = "secondary"
+  kubernetes {
+    host                   = "https://dummy-helm-host-secondary"
+    cluster_ca_certificate = "dummy-helm-ca-secondary"
+    token                  = "dummy-helm-token-secondary"
+  }
+}
 
 # data "aws_eks_cluster_auth" "main" {
 #   name     = aws_eks_cluster.main.name
