1a6afc64a2a573da9f827bc2a094a397981f214b
TemitopeAde22 <temitope224468@gmail.com>
2025-08-07 22:00:08 +0100

    Added EKS Modules

--- Terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-auraflow-app"
-    key            = "terraform.tfstate"
-    region         = "eu-west-2"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,21 +1,4 @@
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0" 
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23" 
-    }
-    helm = {
-      source = "hashicorp/helm"
-      version = "3.0.2"
-    }
-  }
-}
-
 provider "aws" {
   region = var.primary_region
   alias  = "primary" # Alias to distinguish this provider instance
