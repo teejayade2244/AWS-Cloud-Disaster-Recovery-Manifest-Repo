c0ff89c839c7c7159b99d4d19e51bb67a2c89e25
TemitopeAde22 <temitope224468@gmail.com>
2025-08-07 17:41:13 +0100

    first commit

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,4 +1,13 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Use a compatible version, e.g., latest 5.x
+    }
+  }
+}
+
 provider "aws" {
   region = var.primary_region
   alias  = "primary"
