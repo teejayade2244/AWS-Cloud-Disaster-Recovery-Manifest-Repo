0726cdee5147f482ae86a6c83609c94b71080553
TemitopeAde22 <temitope224468@gmail.com>
2025-08-07 22:58:08 +0100

    Added EKS Modules

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     aws = {
@@ -11,23 +10,21 @@ terraform {
     }
     helm = {
       source = "hashicorp/helm"
-      version = "3.0.2"
+      version = "2.12.1" 
     }
   }
 }
 
 provider "aws" {
   region = var.primary_region
-  alias  = "primary" # Alias to distinguish this provider instance
+  alias  = "primary"
 }
 
-# Configure the AWS provider for the secondary region
 provider "aws" {
   region = var.secondary_region
-  alias  = "secondary" # Alias to distinguish this provider instance
+  alias  = "secondary"
 }
 
-# Configure the TLS provider for OIDC thumbprint
 provider "tls" {
   alias = "primary"
 }
@@ -36,25 +33,21 @@ provider "tls" {
   alias = "secondary"
 }
 
-# Configure the Kubernetes provider for the primary EKS cluster
-# This provider's configuration will be dynamically set after the EKS cluster is created
-# It needs a dummy config for `terraform init` to pass
 provider "kubernetes" {
-  alias = "primary"
+  alias                  = "primary"
   host                   = "https://dummy-host-primary"
   cluster_ca_certificate = "dummy-ca-primary"
   token                  = "dummy-token-primary"
 }
 
-# Configure the Kubernetes provider for the secondary EKS cluster
 provider "kubernetes" {
-  alias = "secondary"
+  alias                  = "secondary"
   host                   = "https://dummy-host-secondary"
   cluster_ca_certificate = "dummy-ca-secondary"
   token                  = "dummy-token-secondary"
 }
 
-# Configure the Helm provider for the primary EKS cluster
+# Updated Helm provider configuration using argument syntax
 provider "helm" {
   alias = "primary"
   kubernetes {
@@ -64,7 +57,6 @@ provider "helm" {
   }
 }
 
-# Configure the Helm provider for the secondary EKS cluster
 provider "helm" {
   alias = "secondary"
   kubernetes {
@@ -73,8 +65,3 @@ provider "helm" {
     token                  = "dummy-helm-token-secondary"
   }
 }
-
-# data "aws_eks_cluster_auth" "main" {
-#   name     = aws_eks_cluster.main.name
-# }
-
