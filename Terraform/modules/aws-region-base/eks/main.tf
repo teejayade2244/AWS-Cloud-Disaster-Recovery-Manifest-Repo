8a46c023ae524693595412ba513f3e4cd06e9da5
TemitopeAde22 <temitope224468@gmail.com>
2025-08-10 12:03:30 +0100

    Configured RDS primary DB and Replicas

--- Terraform/modules/aws-region-base/eks/main.tf
+++ Terraform/modules/aws-region-base/eks/main.tf
@@ -2,14 +2,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 5.0"
-      configuration_aliases = [aws]
+      configuration_aliases = [aws, aws.primary, aws.secondary]
     }
     tls = {
-      source  = "hashicorp/tls"
+      source = "hashicorp/tls"
       version = "~> 4.0"
-      configuration_aliases = [tls]
+      configuration_aliases = [tls, tls.primary, tls.secondary]
     }
   }
 }
